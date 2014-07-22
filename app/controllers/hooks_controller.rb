5f51cff599426fbdcb721aad636ba94287c97db8
Daniel Jacob Archer <me@danieljacobarcher.com>
2014-07-22 00:46:06 -0700

    testing early hooks

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -5,6 +5,13 @@ class HooksController < ApplicationController
 	end
 
 	def new_user_info_callback
+	    if params[:results].present?
+	      
+	    end
 
+	    # The webhook doesn't require a response but let's make sure
+	    # we don't send anything
+	    render :nothing => true
+	  end
 	end
 end
